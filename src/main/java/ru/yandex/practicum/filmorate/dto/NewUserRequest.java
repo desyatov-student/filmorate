b5ff3de4342732315379f4234760c55a295361a4
Alexander Desyatov <desyatovav@yandex.ru>
2024-07-09 00:03:07 +0300

    Add db test

--- src/main/java/ru/yandex/practicum/filmorate/dto/NewUserRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/NewUserRequest.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class NewUserRequest {
     @NotBlank(message = "Не должно быть пустым")
     @Email(message = "Должен быть корректный e-mail адрес")
