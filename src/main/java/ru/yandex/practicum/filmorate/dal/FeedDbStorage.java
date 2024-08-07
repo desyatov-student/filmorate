22cd4f53076c9f776ea9128a996251b12b1022bb
danila <y-zelenov@mail.ru>
2024-08-07 10:52:46 +0300

    add feed storage

---
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FeedDbStorage.java
@@ -0,0 +1,40 @@
+package ru.yandex.practicum.filmorate.dal;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.dto.FeedDto;
+import ru.yandex.practicum.filmorate.model.Feed;
+import ru.yandex.practicum.filmorate.storage.FeedStorage;
+
+import java.util.List;
+
+@Repository
+public class FeedDbStorage extends BaseDbStorage<Feed> implements FeedStorage {
+    private static final String FIND_FEED_BY_ID_QUERY = "SELECT * FROM feed where user_id = ?";
+    private static final String PUT_EVENT_QUERY = """ 
+            INSERT INTO feed (timestamp, user_id, event_type, event_id, entity_id)
+            VALUES (?, ?, ?, ?, ?)
+            """;
+
+    public FeedDbStorage(JdbcTemplate jdbc, RowMapper<Feed> mapper) {
+        super(jdbc, mapper);
+    }
+
+    @Override
+    public List<Feed> getFeed(Long userId) {
+        return findMany(FIND_FEED_BY_ID_QUERY, userId);
+    }
+
+    @Override
+    public void save(FeedDto feed) {
+        insert(
+                PUT_EVENT_QUERY,
+                feed.getTimestamp(),
+                feed.getUserId(),
+                feed.getEventType(),
+                feed.getEventId(),
+                feed.getEntityId()
+        );
+    }
+}
