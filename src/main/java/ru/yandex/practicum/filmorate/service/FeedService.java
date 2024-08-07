b1f88a8663e5b5e5ffd393108f3906e9a0d9e53f
danila <y-zelenov@mail.ru>
2024-08-07 10:53:50 +0300

    add feed service

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dto.FeedDto;
+import ru.yandex.practicum.filmorate.mappers.FeedMapper;
+import ru.yandex.practicum.filmorate.mappers.FeedMapperImpl;
+import ru.yandex.practicum.filmorate.storage.FeedStorage;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FeedService {
+    private final FeedMapper feedMapper = new FeedMapperImpl();
+    private final FeedStorage feedStorage;
+
+    public List<FeedDto> getFeed(Long id) {
+        return feedStorage.getFeed(id).stream().map(feedMapper::toDto).toList();
+    }
+}
