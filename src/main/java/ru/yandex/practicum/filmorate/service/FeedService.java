5ca7f1e991c5bce9d0d2903b23e7dd21c27ed74c
Alexander Desyatov <desyatovav@yandex.ru>
2024-08-13 22:49:31 +0300

    Update FeedService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.service;
 
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
