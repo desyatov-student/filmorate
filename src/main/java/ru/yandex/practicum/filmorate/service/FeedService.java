da908cb423897af9b196efc1c91b9e3349510a22
Alexander Desyatov <desyatovav@yandex.ru>
2024-08-13 21:35:34 +0300

    Update FeedService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
@@ -1,6 +1,9 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dto.FeedDto;
 import ru.yandex.practicum.filmorate.mappers.FeedMapper;
@@ -8,6 +11,7 @@ import ru.yandex.practicum.filmorate.mappers.FeedMapperImpl;
 import ru.yandex.practicum.filmorate.model.EventType;
 import ru.yandex.practicum.filmorate.model.Feed;
 import ru.yandex.practicum.filmorate.model.Operation;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FeedStorage;
 
 import java.time.Instant;
@@ -18,16 +22,24 @@ import java.util.List;
 public class FeedService {
     private final FeedStorage feedStorage;
     private final FeedMapper feedMapper = new FeedMapperImpl();
+    private UserService userService;
 
-    public List<FeedDto> getFeeds(Long id) {
-        return feedStorage.getFeed(id).stream().map(feedMapper::toDto).toList();
+    @Autowired
+    void setUserService(@Lazy UserService userService) {
+        this.userService = userService;
+    }
+
+    public List<FeedDto> getFeeds(Long userId) {
+        User user = userService.getUserById(userId);
+        return feedStorage.getFeed(user.getId()).stream().map(feedMapper::toDto).toList();
     }
 
     public void create(Long userId, EventType eventType, Operation operation, Long entityId) {
+        User user = userService.getUserById(userId);
         Feed feed = new Feed(
                 null,
                 Instant.now().toEpochMilli(),
-                userId,
+                user.getId(),
                 eventType,
                 operation,
                 entityId);
